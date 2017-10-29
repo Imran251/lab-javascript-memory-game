f3d2432890f04ee35220a49e1eb8b7f24a798373
Imran Syed <syed.imran2512@gmail.com>
2017-10-29 10:39:46 -0400

    done

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -57,12 +57,4 @@ $(document).ready(function(){
     html += '</div>';
   });
 
-  // Add all the divs to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-  allTheProducts = document.getElementsByClassName('product-row');
-  var price = parseInt(((allTheProducts[productId-1].getElementsByClassName('price'))[0].innerHTML).substring(1));
-  var quantity = parseInt(((allTheProducts[productId-1].getElementsByClassName('quantity'))[0].value));
-  //(allTheProducts[productId-1].getElementsByClassName('sub-total'))[0].innerHTML = "$"+price*quantity;
-  (allTheProducts[productId-1].getElementsByClassName('sub-total'))[0].innerHTML = "$"+price*quantity;
-  return price*quantity;
 });
